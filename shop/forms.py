418f05f3ffcb404a704051b74fa89e9a7c98d266
Nahidul Arafat <nahidularaf@gmail.com>
2026-06-18 02:02:39 +0600

    rating added in form

--- shop/forms.py
+++ shop/forms.py
@@ -1,4 +1,24 @@
 from django import forms
-from django.contrib.auth.forms import UsercreationForm
+from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth.models import User
 from .models import Rating,Order
+
+
+class RegistrationForm(UserCreationForm):
+    email = forms.EmailField(required=True)
+    first_name = forms.CharField(max_length=30, required=False, help_text='Optional.')
+    last_name = forms.CharField(max_length=30, required=False, help_text='Optional.')
+
+    class Meta:
+        model = User
+        fields = ['username', 'email', 'password1', 'password2']
+
+class RatingForm(forms.ModelForm):
+    class Meta:
+        model = Rating
+        fields = ['rating', 'comment']
+        widgets = {
+            'rating': forms.NumberInput(attrs={'min': 1, 'max': 5}),
+            'comment': forms.Textarea(attrs={'rows': 4}),
+        }        
+        
