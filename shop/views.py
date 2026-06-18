e216b136af983c29578980c795f90a155ca0aad0
Nahidul Arafat <nahidularaf@gmail.com>
2026-06-19 01:56:31 +0600

    login_reg_logout_views_added

--- shop/views.py
+++ shop/views.py
@@ -1,3 +1,30 @@
-from django.shortcuts import render
-
+from django.shortcuts import render, redirect,get_object_or_404
+from django.contrib.auth import authenticate, login, logout
 # Create your views here.
+from .models import Product, category, cart, CartItem, Order, Rating
+from .froms import ResgistrationForm, LoginForm, CartItemForm, OrderForm
+def login_view(request):
+    if request.method == 'POST':
+        username = request.POST.get('username')
+        password = request.POST.get('password')
+        # Check database for the user
+        user = authenticate(request, username=username, password=password)
+        if user is not None:
+            login(request, user)
+            return redirect('home')
+        else:
+            return render(request, '', {'error': 'Invalid credentials'})
+    return render(request, '')
+
+def register_view(request):
+    if request.method == 'POST':
+        form = ResgistrationForm(request.POST)
+        if form.is_valid():
+            user=form.save()
+            login(request, user)
+            messages.success(request, 'Registration successful. You are now logged in.')
+            return redirect('home')
+        
+def logout_view(request):
+    logout(request)
+    return redirect('home')            
